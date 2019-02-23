8b460b87044c90a78cdd11c7141c22573a1f27ca
laihuanmin <lai_huanmin@163.com>
2019-02-24 04:24:06 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
+import routes from './router.js';
 
 Vue.use(Vuex);
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes 
+});
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app');
