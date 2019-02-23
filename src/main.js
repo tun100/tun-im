c45734dfef7ec42aad7beef9f9ca52dac6edc60b
laihuanmin <lai_huanmin@163.com>
2019-02-24 04:30:12 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js';
 import store from './store.js';
+import _ from 'lodash';
+import moment from 'moment';
+import flag from './flag'
 
-Vue.config.productionTip = false
+if(flag.isDev()){
+  window._ = _;
+  window.moment = moment;
+}
 
 new Vue({
   render: h => h(App),
