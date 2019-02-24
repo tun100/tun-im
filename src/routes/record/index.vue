29d9b60359fe1a5a85e419bb8b7708383afec44a
laihuanmin <lai_huanmin@163.com>
2019-02-24 18:58:12 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -14,7 +14,7 @@
       <div class="content has-text-centered">
         <p>
           <strong>tun.im</strong> by
-          <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+          <a href="https://jgthms.com">tun100</a>. The source code is licensed
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
           is licensed
           <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
