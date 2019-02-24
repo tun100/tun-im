ee3e945bbfc9a55862bc0464b7608b03ef41066e
laihuanmin <lai_huanmin@163.com>
2019-02-24 20:16:07 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/cpt.js
+++ src/cpt.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import utils from './utils';
 
-// var cptlist = utils.getPathDirList('./components');
-// _.forEach(cptlist,(x,d,n)=>{
-
-// })
+var cptlist = utils.getPathDirList(require.context('./components'));
+debugger;
+_.forEach(cptlist,(x,d,n)=>{
+    Vue.component(x.basedir,x.cpt);
+});
 
 export default {}
