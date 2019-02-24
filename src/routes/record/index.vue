ab9adb62dc9444405ef62212c9ae9ebe767bf4e3
laihuanmin <lai_huanmin@163.com>
2019-02-24 21:47:06 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -12,7 +12,7 @@
     <div class="section tun-nopd tun-tab-wrapper">
       <TunTabs :list="tablist">
         <div v-for="(x,d) in querylist" :slot="d" :key="d">
-          <TunTable v-bind="x" />
+          <TunTable v-bind="x"/>
         </div>
       </TunTabs>
     </div>
