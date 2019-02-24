102f16e5a83e666f39bf64573aca76e7d3890261
laihuanmin <lai_huanmin@163.com>
2019-02-24 20:33:13 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/components/TunTabs/index.vue
+++ src/components/TunTabs/index.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="tabs">
-    <ul>
-      <li class="is-active">
-        <a>Pictures</a>
-      </li>
-      <li>
-        <a>Music</a>
-      </li>
-      <li>
-        <a>Videos</a>
-      </li>
-      <li>
-        <a>Documents</a>
-      </li>
-    </ul>
+  <div class="tun-tabs-wrapper">
+    <div class="tabs">
+      <ul>
+        <li :class="" v-for="(x,d) in list" :key="d">
+          <a>Pictures</a>
+        </li>
+        <li>
+          <a>Music</a>
+        </li>
+        <li>
+          <a>Videos</a>
+        </li>
+        <li>
+          <a>Documents</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    list: {}
+  }
+};
+</script>
