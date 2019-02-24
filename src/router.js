18b9869712bc24d7a0e4202273357a80355216a8
laihuanmin <lai_huanmin@163.com>
2019-02-24 20:06:11 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/router.js
+++ src/router.js
@@ -1,41 +1,53 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import _ from 'lodash';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import _ from 'lodash'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
-const IndexPath = 'record';
-var ctxpath = "./routes";
-var ctx = require.context(ctxpath);
-var keys = ctx.keys();
-var pathStrList = _.chain(keys).map(x=>{
-    x = _.replace(x,/\/$/,x=>'');
-    return _.split(x,'/');
-}).filter(x=>_.size(x) == 2).map(x=>_.join(x,'/')).uniq().value();
-var routes = _.chain(pathStrList).map(x=>{
-    var patharr = _.split(x,'/');
-    var basedir = patharr[1];
-    var cptpath = x + '/index.vue';
-    var cpt = ctx(cptpath).default;
-    var result  = {
-        component: cpt,
-        name: basedir,
-        path: '/'+basedir
-    }
-    if(basedir == IndexPath){
-        return [
-            result,
-            {
-                ...result,
-                path: '/',
-                name: 'root'
-            }
-        ]
-    }
-    return result;
-}).flattenDeep().value();
+const IndexPath = 'record'
+var ctxpath = './routes'
+var ctx = require.context(ctxpath)
+var keys = ctx.keys()
+var pathDirList = _.chain(keys)
+	.map(x => {
+  x = _.replace(x, /\/$/, x => '')
+  return _.split(x, '/')
+})
+	.filter(x => _.size(x) == 2)
+	.map(x => _.join(x, '/'))
+	.uniq()
+	.map(x => {
+  var patharr = _.split(x, '/')
+  var basedir = patharr[1]
+  var cptpath = x + '/index.vue'
+  var cpt = ctx(cptpath).default
+  return { patharr, basedir, cptpath, cpt }
+})
+	.value();
+var routes = _.chain(pathDirList)
+	.map(x => {
+  var { patharr, basedir, cptpath, cpt } = x
+  var result = {
+    component: cpt,
+    name: basedir,
+    path: '/' + basedir
+  }
+  if (basedir == IndexPath) {
+    return [
+      result,
+      {
+        ...result,
+        path: '/',
+        name: 'root'
+      }
+    ]
+  }
+  return result
+})
+	.flattenDeep()
+	.value()
 const router = new VueRouter({
- routes 
-});
+  routes
+})
 
-export default router;
+export default router
