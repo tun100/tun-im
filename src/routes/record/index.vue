375aeca2227630a6f712f04d3d9a1288e45b6eab
laihuanmin <lai_huanmin@163.com>
2019-02-24 21:57:07 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -10,9 +10,7 @@
       </div>
     </section>
     <div class="section tun-nopd tun-tab-wrapper">
-        <div v-for="(x,d) in querylist" :slot="d" :key="d">
-           {{d}}
-        </div>
+      <div v-for="(x,d) in querylist" :slot="d" :key="d">{{d}}</div>
       <TunTabs :list="tablist">
         <div v-for="(x,d) in querylist" :slot="d" :key="d">
           <TunTable v-bind="x"/>
@@ -24,7 +22,10 @@
         <p>
           本网站所有源码已同步至
           <a target="_blank" href="https://github.com/tun100/tun-im">Github仓库</a>里，使用了bulma的CSS框架以及
-          <a target="_blank" href="https://github.com/tun100/tun-im/blob/master/package.json">相关依赖</a>，并采用travis实现自动持续部署。
+          <a
+            target="_blank"
+            href="https://github.com/tun100/tun-im/blob/master/package.json"
+          >相关依赖</a>，并采用travis实现自动持续部署。
         </p>
       </div>
     </footer>
@@ -37,16 +38,21 @@ var rawTabListSource = ["JS前端", "Java后端", "计算机网络", "小工具"
 
 export default {
   data() {
+    var querylist = _.chain(rawTabListSource)
+      .mapKeys((x, d) => x)
+      .mapValues(x => ({
+        column: _.map(
+          ["记录类型", "记录名称", "记录描述", "添加日期", "操作"],
+          x => ({ label: x, value: x })
+        ),
+        data: [],
+        ctn: 0
+      }))
+      .value();
+      debugger;
     return {
       dateformat: this.getDateFormat(),
-      querylist: _.chain(rawTabListSource)
-        .mapKeys((x, d) => d)
-        .mapValues(x => ({
-          column: _.map(['记录类型',"记录名称", "记录描述", "添加日期", "操作"],x=>({label:x,value:x})),
-          data: [],
-          ctn: 0
-        }))
-        .value()
+      querylist
     };
   },
   computed: {
