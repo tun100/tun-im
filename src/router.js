393f3de116897bb9807ef9e80ab1bb2707fb6286
laihuanmin <lai_huanmin@163.com>
2019-02-24 09:32:06 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import VueRouter from 'vue-router';
 import _ from 'lodash';
 
-var ctx = require.context("./routes").keys();
-var routesList = _.chain(ctx).map(x=>{
+var ctx = require.context("./routes");
+var keys = ctx.keys();
+var routesList = _.chain(keys).map(x=>{
     x = _.replace(x,/\/$/,x=>'');
     return _.split(x,'/');
 }).filter(x=>_.size(x) == 2).map(x=>_.join(x,'/')).uniq().value();
-console.log(ctx);
+console.log(routesList);
 // debugger;
 const router = new VueRouter({
   
