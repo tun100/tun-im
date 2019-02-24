048f2331a0625338fa4b75b6ca2733c4f86567d0
laihuanmin <lai_huanmin@163.com>
2019-02-24 13:16:06 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -1,32 +1,5 @@
 <template>
   <div>
-    <h1 class="title">Bulma</h1>
-
-    <p class="subtitle">Modern CSS framework based on
-      <a
-        href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox"
-      >Flexbox</a>
-    </p>
-
-    <div class="field">
-      <div class="control">
-        <input class="input" type="text" placeholder="Input">
-      </div>
-    </div>
-
-    <div class="field">
-      <p class="control">
-        <span class="select">
-          <select>
-            <option>Select dropdown</option>
-          </select>
-        </span>
-      </p>
-    </div>
-
-    <div class="buttons">
-      <a class="button is-primary">Primary</a>
-      <a class="button is-link">Link</a>
-    </div>
+    <h1 class="title">TUN - 记录站</h1>
   </div>
 </template>
