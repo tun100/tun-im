deb569f08afb78341a10d664a1b4aee4dbb33ccc
laihuanmin <lai_huanmin@163.com>
2019-02-24 21:54:08 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/components/TunTable/index.vue
+++ src/components/TunTable/index.vue
@@ -1,11 +1,21 @@
 <template>
     <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-
+        <thead>
+            <th>
+                <td v-for="(cx,cd) in column" :key="cd">{{cx.label}}</td>
+            </th>
+        </thead>
+        <tbody>
+            <tr v-for="(x,d) in data" :key="d">
+                <td v-for="(cx,cd) in column" :key="cd">{{data[cx.value]}}</td>
+            </tr>
+        </tbody>
     </table>
 </template>
 <script>
 export default {
     props: {
+        column: {},
         data:{},
         ctn: {}
     }
