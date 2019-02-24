8bb1be119af6abd352c81d006d2761b5fd2e92fb
laihuanmin <lai_huanmin@163.com>
2019-02-24 18:14:06 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="container">
-        <div class="navbar">navbar</div>
-        <div class="hero">hero</div>
-        <div class="section">section</div>
-        <div class="footer">fotter</div>
-    </div>
+  <div class="container">
+    <div class="navbar">navbar</div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">Hero title</h1>
+          <h2 class="subtitle">Hero subtitle</h2>
+        </div>
+      </div>
+    </section>
+    <div class="section">section</div>
+    <div class="footer">fotter</div>
+  </div>
 </template>
 <script>
-export default {
-
-};
+export default {};
 </script>
 <style>
 </style>
