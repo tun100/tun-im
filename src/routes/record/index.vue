b95409430b139781fd16cfeee3357fc5bfa4cce1
laihuanmin <lai_huanmin@163.com>
2019-02-24 18:47:07 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -4,7 +4,7 @@
     <section class="hero is-dark">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">热爱技术，记录下每刻学习历程</h1>
+          <h1 class="title">不断学习！记录下每刻学习历程</h1>
           <h2 class="subtitle">今天是2019年12月10日 广州 晴</h2>
         </div>
       </div>
