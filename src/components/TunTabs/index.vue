c6b1840e474d2c427605ff9be7898d2e4bd7dde7
laihuanmin <lai_huanmin@163.com>
2019-02-24 20:21:11 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/components/TunTabs/index.vue
+++ src/components/TunTabs/index.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        tun tab
-    </div>
+  <div class="tabs">
+    <ul>
+      <li class="is-active">
+        <a>Pictures</a>
+      </li>
+      <li>
+        <a>Music</a>
+      </li>
+      <li>
+        <a>Videos</a>
+      </li>
+      <li>
+        <a>Documents</a>
+      </li>
+    </ul>
+  </div>
 </template>
