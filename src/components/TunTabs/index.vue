e750403277d6286b343a9d45db34cfb066bad56e
laihuanmin <lai_huanmin@163.com>
2019-02-24 20:36:07 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/components/TunTabs/index.vue
+++ src/components/TunTabs/index.vue
@@ -2,7 +2,12 @@
   <div class="tun-tabs-wrapper">
     <div class="tabs">
       <ul>
-        <li @click="tmpactive = x.value" :class="x.value == active ? 'is-active':''" v-for="(x,d) in list" :key="d">
+        <li
+          @click="tmpactive = x.value"
+          :class="x.value == active ? 'is-active':''"
+          v-for="(x,d) in list"
+          :key="d"
+        >
           <a>{{x.label}}</a>
         </li>
       </ul>
@@ -10,11 +15,27 @@
   </div>
 </template>
 <script>
+import _ from "lodash";
+
 export default {
   data() {
     return {
       tmpactive: undefined
     };
+  },
+  computed: {
+    active() {
+      var { tmpactive } = this;
+      if (
+        _.isNil(tmpactive) ||
+        _.isNil(_.find(this.list, x => x.value == tmpactive))
+      ) {
+        if (!_.isEmpty(this.list)) {
+          tmpactive = this.tmpactive = _.get(this, ["list", 0, "value"]);
+        }
+      }
+      return tmpactive;
+    }
   },
   props: {
     list: {}
