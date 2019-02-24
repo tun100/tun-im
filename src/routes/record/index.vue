d1d8d60a6f3ae15b2f7af62882d79f645b0701e2
laihuanmin <lai_huanmin@163.com>
2019-02-24 20:28:14 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/routes/record/index.vue
+++ src/routes/record/index.vue
@@ -15,7 +15,7 @@
     <footer class="footer tun-footer">
       <div class="content has-text-centered">
         <p>本网站所有源码已上传至
-          <a href="https://github.com/tun100/tun-im">Github仓库</a>里，搭配Travis自动构建工具实时持续部署
+          <a target="_blank" href="https://github.com/tun100/tun-im">Github仓库</a>里，并搭配Travis自动构建工具实时持续部署
         </p>
       </div>
     </footer>
