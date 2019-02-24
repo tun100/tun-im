04016ad3fca8468478de018fb257498699ad18c1
laihuanmin <lai_huanmin@163.com>
2019-02-24 22:07:12 +0800

    SYSTEM AUTO COMMIT to run travis task, 系统自动提交以方便实时travis编译

--- src/components/TunTable/index.vue
+++ src/components/TunTable/index.vue
@@ -9,18 +9,24 @@
             <tr v-for="(x,d) in data" :key="d">
                 <td v-for="(cx,cd) in column" :key="cd">{{data[cx.value]}}</td>
             </tr>
-            <tr v-for="(x,d) in 1" :key="d" v-if="data.length == 0">
+            <tr v-for="(x,d) in createIfDataIsEmpty" :key="d" >
                 <td v-for="(cx,cd) in column" :key="cd">{{data[cx.value]}}</td>
             </tr>
         </tbody>
     </table>
 </template>
 <script>
+import _ from 'lodash';
 export default {
     props: {
         column: {},
         data:{},
         ctn: {}
+    },
+    computed:{
+        createIfDataIsEmpty(){
+            return _.isEmpty(this.data) ? 1:0;
+        }
     }
 };
 </script>
